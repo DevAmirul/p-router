ee03636b4a248e4376c4f100bf420f3d05b7c7d5
Amirul islam <dev.amirul@gmail.com>
2023-10-19 23:26:00 +0600

    updated composer

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// Require composer autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+$router = new Devamirul\PRouter\Router();
+
+
+
+
+
+
+
+$router->resolve();
---
+++ src/Router.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Devamirul\PRouter;
+
+class Router {
+
+}
